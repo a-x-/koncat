d880f17fce3ec471e0bd66e449daa1d18f2f53e4
Alexander Mekhonoshin (invntrm) <invntrm@yandex-team.ru>
2016-07-31 22:14:05 +0300

    fix

--- bin.js
+++ bin.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const koncat = require('.')
 const getopt = require('commander')
+const fs = require('fs')
 
 getopt
   .version(require('./package.json').version)
@@ -9,7 +10,7 @@ getopt
   .option('-t, --title [mask]', 'content delimeter; e.g. // file: {path}')
   .parse(process.argv)
 
-getopt.input = getopt.args || fs.readFileSync('/dev/stdin', 'utf8').split('\n|\s+')
+getopt.input = getopt.args.length ? getopt.args : fs.readFileSync('/dev/stdin', 'utf8').split(/\n|\s+/).map(path=>path.trim())
 
 koncat({
   inputFiles: Array.isArray(getopt.input) ? getopt.input : [getopt.input],
