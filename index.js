51b830215b2713b3ee037b39dd75aa2e345c9b76
Александр invntrm@ <a-x-@users.noreply.github.com>
2016-07-31 21:53:34 +0400

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const fs = require('fs')
 const helpers = require('broccoli-kitchen-sink-helpers')
 
 module.exports = function (options) {
+  const getDefaultTransform = title => function defaultTransform(path, content) {
+    var out = [];
+    getopt.title !== undefined && out.push('\n' + (title || '//< file: {path}').replace('{path}', path));
+    out.push(content);
+    return out.join('\n');
+  }
+  
   const inputFiles = options.inputFiles;
   const outputFile = options.outputFile;
-  const transform = options.transform;
+  const transform = typeof options.transform === 'string' ? getDefaultTransform(options.transform) : options.transform;
 
   (function concat () {
     const inputFiles = helpers.multiGlob(inputFiles, { cwd: process.cwd() })
